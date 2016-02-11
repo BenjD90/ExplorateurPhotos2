ff45c514148af4ae5ca9720862d014146e4125a9
Benjamin <BenjDaniel90@gmail.com>
2016-02-11 14:18:20 +0100

    [STRUCTURE] Add yo angular generation, serve in Chrome

---
+++ HTML/app/scripts/app.js
@@ -0,0 +1,36 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name htmlApp
+ * @description
+ * # htmlApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('htmlApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
