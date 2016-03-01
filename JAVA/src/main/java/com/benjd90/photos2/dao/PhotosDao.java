62b0dfacc28004328516b1ec1438d326c8561fef
Benjamin <BenjDaniel90@gmail.com>
2016-03-01 17:23:46 +0100

    [HTML] Justify view of photos

--- JAVA/src/main/java/com/benjd90/photos2/dao/PhotosDao.java
+++ JAVA/src/main/java/com/benjd90/photos2/dao/PhotosDao.java
@@ -5,7 +5,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
+import javax.imageio.IIOImage;
 import javax.imageio.ImageIO;
+import javax.imageio.ImageWriteParam;
+import javax.imageio.ImageWriter;
+import javax.imageio.plugins.jpeg.JPEGImageWriteParam;
+import javax.imageio.stream.MemoryCacheImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -40,14 +45,27 @@ public class PhotosDao implements IPhotosDao {
 
       BufferedImage resizedImageHintJpg = PhotosUtils.resizeImage(originalImage, width, height);
 
-      ByteArrayOutputStream baos = new ByteArrayOutputStream();
-      ImageIO.write(resizedImageHintJpg, "jpg", baos);
-      baos.flush();
-      byte[] imageInByte = baos.toByteArray();
-      baos.close();
+      byte[] imageInByte = getBufferedImageBytes(resizedImageHintJpg);
       return imageInByte;
     } else {
       return null;
     }
+  }
+
+  private byte[] getBufferedImageBytes(BufferedImage resizedImageHintJpg) throws IOException {
+    JPEGImageWriteParam jpegParams = new JPEGImageWriteParam(null);
+    jpegParams.setCompressionMode(ImageWriteParam.MODE_EXPLICIT);
+    jpegParams.setCompressionQuality(0.5f);
+
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+    final ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
+
+    writer.setOutput(new MemoryCacheImageOutputStream(outputStream));
+    writer.write(null, new IIOImage(resizedImageHintJpg, null, null), jpegParams);
+
+    byte[] imageInByte = outputStream.toByteArray();
+    outputStream.close();
+    return imageInByte;
   }
 }
